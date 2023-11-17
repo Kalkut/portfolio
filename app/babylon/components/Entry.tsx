f465b1a12f6873d8c5c05914d509fd4a1e454586
Kalkut <berenger.makita@gmail.com>
2023-11-17 22:32:17 +0100

    dynamic Entry size

--- app/babylon/components/Entry.tsx
+++ app/babylon/components/Entry.tsx
@@ -6,6 +6,7 @@ import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
 import { Texture } from "@babylonjs/core/Materials/Textures/texture";
 import { useContext, useEffect, useState } from "react";
 import { SceneContext } from "../App";
+import { VideoTexture } from "@babylonjs/core/Materials/Textures/videoTexture";
 
 export function Entry({
   preview,
@@ -18,38 +19,40 @@ export function Entry({
   position?: Vector3;
   rotation?: Vector3;
 }) {
+  const [texture, setTexture] = useState<Texture>();
   const [mesh, setMesh] = useState<Mesh>();
   const scene = useContext(SceneContext);
 
   useEffect(() => {
-    const material = new StandardMaterial("Entry", scene);
-    if(type === "image") {
-      const texture = new Texture(preview, scene);
-      material.diffuseTexture = texture;
-      material.emissiveTexture = texture;
-      texture.onLoadObservable.addOnce(() => {
-        const size = texture.getSize();
-        console.log(size, "size");
-      });
+    const isImg = type === "image";
+    const _texture = isImg ? new Texture(preview, scene) : new VideoTexture("Video preview", preview, scene);
+    _texture.onLoadObservable.addOnce(() => {
+      setTexture(_texture);
+    });
+
+    return () => {
+      _texture.dispose();
     }
   }, [preview, type, scene]);
 
   useEffect(() => {
-    const _mesh = MeshBuilder.CreatePlane("entry", {}, scene);
-    
+    if(!texture) return;
+    const size = texture.getSize();
+    console.log(size, "size");
+    const width = size.width / size.height;
+    const _mesh = MeshBuilder.CreatePlane("entry", { width, height: 1}, scene);
     const material = new StandardMaterial("Entry", scene);
-    if(preview) {
-      const texture = new Texture(preview, scene);
-      material.diffuseTexture = texture;
-      material.emissiveTexture = texture;
-    }
+    material.diffuseTexture = texture;
+    material.emissiveTexture = texture;
     
     _mesh.material = material;
     setMesh(_mesh);
+
     return () => {
       _mesh.dispose();
+      material.dispose();
     }
-  }, [scene, preview]);
+  }, [scene, texture]);
   
   // Position update
   useEffect(() => {
