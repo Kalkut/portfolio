c48634a55cf023dc2aa7b4562b24812e907000ac
Kalkut <berenger.makita@gmail.com>
2023-11-18 15:04:44 +0100

    hoverable Entry

--- app/babylon/components/Entry.tsx
+++ app/babylon/components/Entry.tsx
@@ -7,6 +7,8 @@ import { Texture } from "@babylonjs/core/Materials/Textures/texture";
 import { useContext, useEffect, useState } from "react";
 import { SceneContext } from "../App";
 import { VideoTexture } from "@babylonjs/core/Materials/Textures/videoTexture";
+import { ActionManager } from "@babylonjs/core/Actions/actionManager";
+import { ExecuteCodeAction } from "@babylonjs/core";
 
 export function Entry({
   preview,
@@ -21,11 +23,15 @@ export function Entry({
 }) {
   const [texture, setTexture] = useState<Texture>();
   const [mesh, setMesh] = useState<Mesh>();
+  const [hovered, setHovered] = useState(false);
   const scene = useContext(SceneContext);
 
   useEffect(() => {
     const isImg = type === "image";
-    const _texture = isImg ? new Texture(preview, scene) : new VideoTexture("Video preview", preview, scene,undefined,false,undefined,{ muted: true});
+    const _texture = isImg ?
+    new Texture(preview, scene) :
+    new VideoTexture("Video preview", preview, scene,undefined,false,undefined,{ muted: true});
+
     _texture.onLoadObservable.addOnce(() => {
       setTexture(_texture);
     });
@@ -38,7 +44,7 @@ export function Entry({
   useEffect(() => {
     if(!texture) return;
     const size = texture.getSize();
-    console.log(size, "size");
+    console.log(size, "size", texture);
     const width = size.width / size.height;
     const _mesh = MeshBuilder.CreatePlane("entry", { width, height: 1}, scene);
     const material = new StandardMaterial("Entry", scene);
@@ -60,14 +66,40 @@ export function Entry({
 
     if(position) mesh.position = position;
     if(rotation) mesh.rotation = rotation;
-  }, [mesh, position, rotation])
+  }, [mesh, position, rotation]);
+
+  useEffect(() => {
+    if(!hovered) return;
+    document.body.classList.add("pointer");
+
+    return () => {
+      document.body.classList.remove("pointer");
+    }
+  }, [hovered]);
 
   // On Destroy
   useEffect(() => {
+    if(!mesh) return;
+    
+    const hover = new ExecuteCodeAction(
+      { trigger: ActionManager.OnPointerOverTrigger, target: mesh},
+      () => setHovered(true),
+    );
+    const blur = new ExecuteCodeAction(
+      { trigger: ActionManager.OnPointerOutTrigger, target: mesh},
+      () => setHovered(false),
+    );
+
+    const hoverable = hover.then(blur);
+
+    const actionManager = new ActionManager(scene);
+    actionManager.registerAction(hoverable);
+    mesh.actionManager = actionManager;
+
     return () => {
-      mesh?.dispose(true, true);
+      mesh.dispose(true, true);
     }
-  }, [mesh]);
+  }, [mesh, scene]);
 
   return null;
 }
