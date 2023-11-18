d867a3d983e918c1e5d37bf8d4c6f9dd02bf6495
Kalkut <berenger.makita@gmail.com>
2023-11-18 18:23:16 +0100

    hoverable card

--- app/babylon/components/Card.tsx
+++ app/babylon/components/Card.tsx
@@ -8,7 +8,7 @@ import { useContext, useEffect, useState } from "react";
 import { SceneContext } from "../App";
 import { VideoTexture } from "@babylonjs/core/Materials/Textures/videoTexture";
 import { ActionManager } from "@babylonjs/core/Actions/actionManager";
-import { ExecuteCodeAction } from "@babylonjs/core";
+import { CombineAction, ExecuteCodeAction, InterpolateValueAction, Scene } from "@babylonjs/core";
 
 export function Card({
   preview,
@@ -23,9 +23,9 @@ export function Card({
 }) {
   const [texture, setTexture] = useState<Texture>();
   const [mesh, setMesh] = useState<Mesh>();
-  const [hovered, setHovered] = useState(false);
   const scene = useContext(SceneContext);
 
+  // Texture Load
   useEffect(() => {
     const isImg = type === "image";
     const _texture = isImg ?
@@ -41,26 +41,23 @@ export function Card({
     }
   }, [preview, type, scene]);
 
+  // Card Setup
   useEffect(() => {
     if(!texture) return;
-    const size = texture.getSize();
-    console.log(size, "size", texture);
-    const width = size.width / size.height;
-    const _mesh = MeshBuilder.CreatePlane("Card", { width, height: 1}, scene);
-    const material = new StandardMaterial("Card", scene);
-    material.diffuseTexture = texture;
-    material.emissiveTexture = texture;
     
-    _mesh.material = material;
+    const _mesh = createCardMesh(texture, scene);
+    const _backdrop = createBackdropMesh(texture, scene);
+    _backdrop.setParent(_mesh);
+    _backdrop.position.z = -0.001
+    makeCardHoverable(_mesh, _backdrop, scene);
     setMesh(_mesh);
 
     return () => {
       _mesh.dispose();
-      material.dispose();
     }
   }, [scene, texture]);
-  
-  // Position update
+
+  // Transform update
   useEffect(() => {
     if(!mesh) return;
 
@@ -68,38 +65,59 @@ export function Card({
     if(rotation) mesh.rotation = rotation;
   }, [mesh, position, rotation]);
 
-  useEffect(() => {
-    if(!hovered) return;
-    document.body.classList.add("pointer");
-
-    return () => {
-      document.body.classList.remove("pointer");
-    }
-  }, [hovered]);
-
-  // On Destroy
-  useEffect(() => {
-    if(!mesh) return;
-    
-    const hover = new ExecuteCodeAction(
-      { trigger: ActionManager.OnPointerOverTrigger, target: mesh},
-      () => setHovered(true),
-    );
-    const blur = new ExecuteCodeAction(
-      { trigger: ActionManager.OnPointerOutTrigger, target: mesh},
-      () => setHovered(false),
-    );
-
-    const hoverable = hover.then(blur);
-
-    const actionManager = new ActionManager(scene);
-    actionManager.registerAction(hoverable);
-    mesh.actionManager = actionManager;
-
-    return () => {
-      mesh.dispose(true, true);
-    }
-  }, [mesh, scene]);
 
   return null;
+}
+
+function createCardMesh(texture: Texture, scene: Scene) {
+  const material = new StandardMaterial("Card", scene);
+  material.diffuseTexture = texture;
+  material.emissiveTexture = texture;
+  
+  const mesh = fromTextureToPlane(texture, scene);
+  mesh.material = material;
+  return mesh;
+}
+
+function createBackdropMesh(texture: Texture, scene: Scene) {
+  const backdrop = fromTextureToPlane(texture, scene);
+  const material = new StandardMaterial("Card Backdrop", scene);
+  material.diffuseColor.set(0,0,0);
+  
+  backdrop.material = material;
+  backdrop.visibility = 0;
+  return backdrop;
+}
+
+function fromTextureToPlane(texture: Texture, scene: Scene) {
+  const size = texture.getSize();
+  const width = size.width / size.height;
+  const mesh = MeshBuilder.CreatePlane("Card", { width, height: 1 }, scene);
+  return mesh;
+}
+
+function makeCardHoverable(mesh: Mesh, backdrop: Mesh, scene: Scene) {
+  const addPointer = new ExecuteCodeAction(
+    ActionManager.NothingTrigger,
+    () => {
+      document.body.classList.add("pointer");
+    },
+  );
+  const removePointer = new ExecuteCodeAction(
+    ActionManager.NothingTrigger,
+    () => {
+      document.body.classList.remove("pointer")
+    },
+  );
+
+  const fadeIn = new InterpolateValueAction(ActionManager.NothingTrigger, backdrop, "visibility", 0.6, 200);
+  const fadeOut = new InterpolateValueAction(ActionManager.NothingTrigger, backdrop, "visibility", 0, 200);
+
+  const hover = new CombineAction(ActionManager.OnPointerOverTrigger, [addPointer, fadeIn]);
+  const blur = new CombineAction(ActionManager.OnPointerOutTrigger, [removePointer, fadeOut]);
+
+  const actionManager = new ActionManager(scene);
+  actionManager.registerAction(hover);
+  actionManager.registerAction(blur);
+  mesh.actionManager = actionManager;
 }
