b9244f2cbc6cbe4c38975851ffad9902b90fbefc
Kalkut <berenger.makita@gmail.com>
2023-12-08 15:01:39 +0100

    fix navi menu font size

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function Navigation() {
   return (
     <>
       <nav className="absolute text-white top-0 left-0 p-4 2xl:p-8 sm:text-2xl 2xl:text-3xl flex">
         <Link
-          className="hover:font-semibold text-2xl 2xl:mr-4 hover:2xl:mr-2.5"
+          className="hover:font-semibold mr-4 hover:2xl:mr-2.5"
           href="/projects"
         >
           Projects
         </Link>
         <a
-          className="hover:font-semibold text-2xl 2xl:mr-4 hover:2xl:mr-[0.78rem]"
+          className="hover:font-semibold mr-4 hover:2xl:mr-[0.78rem]"
           href="https://github.com/Kalkut"
         >
           Github
         </a>
         <a
-          className="hover:font-semibold text-2xl 2xl:mr-4"
+          className="hover:font-semibold mr-4"
           href="https://www.linkedin.com/in/beremaki"
         >
           Linkedin
         </a>
-        <Link className="hover:font-semibold text-2xl" href="/about">
+        <Link className="hover:font-semibold" href="/about">
           About
         </Link>
       </nav>
