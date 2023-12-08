5decada70f7e0e52d80fba1a4b4d98e9f240fc9c
Kalkut <berenger.makita@gmail.com>
2023-12-08 13:02:34 +0100

    adjust lights

--- app/babylon/Lights.tsx
+++ app/babylon/Lights.tsx
@@ -1,11 +1,17 @@
 import { useContext, useEffect } from "react";
 import { SceneContext } from "./Canvas";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
 
 export function Lights() {
   const scene = useContext(SceneContext);
 
   useEffect(() => {
-    // scene.createDefaultLight(true);
+    const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.4;
+    return () => {
+      light.dispose();
+    };
   }, [scene]);
 
   return null;
