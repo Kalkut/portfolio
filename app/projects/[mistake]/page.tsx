5f5c2b4aa6d75afc6ac3fa7100fab4f7953621dc
Kalkut <berenger.makita@gmail.com>
2023-12-09 18:41:49 +0100

    not found

---
+++ app/projects/[mistake]/page.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="p-4">
+      <h2 className="text-xl font-semibold mb-8">Project Not Found</h2>
+      <p className="mb-2">Sure of your url ?</p>
+      <p>
+        Nevermind, just try one of the link above or{" "}
+        <Link
+          className="underline"
+          href="/projects/webxr-google-drive-client#description"
+        >
+          check my latest entry out
+        </Link>{" "}
+        !
+      </p>
+    </div>
+  );
+}
